d019bfa248054140985f6f392bb553e168faf3e2
Qixin <Qixin@nsit-dhcp-205-208-055-133.uchicago.edu>
2015-09-16 15:03:43 -0500

    changed mutation and recombination to events, instead of at each transmission point

---
+++ src/Loci.cpp
@@ -0,0 +1,9 @@
+//
+//  Loci.cpp
+//  malariamodel
+//
+//  Created by Qixin on 8/24/15.
+//  Copyright (c) 2015 Ed Baskerville. All rights reserved.
+//
+
+#include "Loci.h"
---
+++ src/Loci.h
@@ -0,0 +1,14 @@
+//
+//  Loci.h
+//  malariamodel
+//
+//  Created by Qixin on 8/24/15.
+//  Copyright (c) 2015 Ed Baskerville. All rights reserved.
+//
+
+#ifndef __malariamodel__Loci__
+#define __malariamodel__Loci__
+
+#include <iostream>
+
+#endif /* defined(__malariamodel__Loci__) */
